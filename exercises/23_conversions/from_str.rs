8a83c8d8fa12657f72c90b1430d0499b466008e2
Arjun Bhamra <arjunbhamra@Arjuns-MacBook-Pro.local>
2024-06-03 20:18:51 -0400

    last of conversions, finished

--- exercises/23_conversions/from_str.rs
+++ exercises/23_conversions/from_str.rs
@@ -31,8 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -48,6 +46,27 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            Err(ParsePersonError::Empty)
+        } else {
+            let mut iter = s.split(",");
+            let name = match iter.next() {
+                Some("") | None => return Err(ParsePersonError::NoName),
+                Some(val) => String::from(val),
+            };
+            let age = match iter.next() {
+                Some(x) => match x.parse::<usize>() {
+                    Ok(x) => x,
+                    Err(err) => return Err(ParsePersonError::ParseInt(err)),
+                },
+                None => return Err(ParsePersonError::BadLen),
+            };
+            match iter.next() {
+                // to catch the edge case of name,age,something_else
+                Some(_) => return Err(ParsePersonError::BadLen),
+                None => Ok(Person { name, age }),
+            }
+        }
     }
 }
 
--- exercises/23_conversions/try_from_into.rs
+++ exercises/23_conversions/try_from_into.rs
@@ -9,7 +9,10 @@
 // Execute `rustlings hint try_from_into` or use the `hint` watch subcommand for
 // a hint.
 
-use std::convert::{TryFrom, TryInto};
+use std::{
+    convert::{TryFrom, TryInto},
+    num::ParseIntError,
+};
 
 #[derive(Debug, PartialEq)]
 struct Color {
@@ -27,8 +30,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
 // integers, an array of three integers, and a slice of integers.
@@ -41,13 +42,33 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        if !bounds(tuple.0) || !bounds(tuple.1) || !bounds(tuple.2) {
+            return Err(IntoColorError::IntConversion);
+        }
+        Ok(Color {
+            red: tuple.0 as u8,
+            green: tuple.1 as u8,
+            blue: tuple.2 as u8,
+        })
     }
+}
+
+fn bounds(i: i16) -> bool {
+    return i <= 255 && i >= 0;
 }
 
 // Array implementation
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        if !bounds(arr[0]) || !bounds(arr[1]) || !bounds(arr[2]) {
+            return Err(IntoColorError::IntConversion);
+        }
+        Ok(Color {
+            red: arr[0] as u8,
+            green: arr[1] as u8,
+            blue: arr[2] as u8,
+        })
     }
 }
 
@@ -55,6 +76,17 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+        if !bounds(slice[0]) || !bounds(slice[1]) || !bounds(slice[2]) {
+            return Err(IntoColorError::IntConversion);
+        }
+        Ok(Color {
+            red: slice[0] as u8,
+            green: slice[1] as u8,
+            blue: slice[2] as u8,
+        })
     }
 }
 
